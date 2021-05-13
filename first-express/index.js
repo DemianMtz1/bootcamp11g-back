67c8ea225a3f80c3784456a07721631454eb7b83
demian <ericdemian.martinez@gmail.com>
2021-05-12 20:34:54 -0500

    ADDED CRUD express+fs

--- first-express/index.js
+++ first-express/index.js
@@ -12,6 +12,7 @@ server.get('/', (req, res) => {
     res.write('GET: Ruta inicial');
     res.end();
 })
+
 server.get('/koders', (req, res) => {
     fs.readFile('koders.json', 'utf-8', (err, data) => {
         if (err) {
@@ -21,12 +22,22 @@ server.get('/koders', (req, res) => {
     })
 })
 
+server.get('/koders/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let allKoders = JSON.parse(fs.readFileSync('koders.json', 'utf-8'));
+
+    let filteredKoder = allKoders.koders.find(koder => koder.id === id)
+
+    res.status(201);
+    res.json(filteredKoder ? filteredKoder: {message: 'Koder no encontrado'})
+})
+
 server.post('/koders', (req, res) => {
     const newKoder = req.body;
     let allKoders = JSON.parse(fs.readFileSync('koders.json', 'utf-8'))
     allKoders.koders.push(newKoder)
-    
-    fs.writeFile('koders.json', JSON.stringify(allKoders) ,err => {
+
+    fs.writeFile('koders.json', JSON.stringify(allKoders, null, 2), err => {
         if (err) {
             console.error('ERROR: ', err)
             res.status(401);
@@ -44,7 +55,67 @@ server.put('/koders', (req, res) => {
     res.write('PUT: Aqui puedes sustituir un koder');
 
     res.end();
-})
+});
+
+server.patch('/koders/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    const name = req.body.name;
+    let allKoders = JSON.parse(fs.readFileSync('koders.json', 'utf-8'));
+
+    let newKoders = allKoders.koders.reduce((koders, koder) => {
+        if (id === koder.id) {
+            koder.name = name;
+        }
+        return [
+            ...koders,
+            koder
+        ]
+    }, []);
+
+    allKoders.koders = newKoders
+
+
+    fs.writeFileSync('koders.json', JSON.stringify(allKoders, null, 2), err => {
+        if (err) {
+            console.error('ERROR: ', err)
+            res.status(401);
+            res.end()
+        }
+    })
+    // escribir usando fs
+    res.status(201);
+    res.json({
+        message: 'ok'
+    })
+});
+
+
+server.delete('/koders/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    const name = req.body.name;
+    let allKoders = JSON.parse(fs.readFileSync('koders.json', 'utf-8'));
+
+    let newKoders = allKoders.koders.filter(koder => koder.id !== id)
+
+    allKoders.koders = newKoders
+
+
+    fs.writeFileSync('koders.json', JSON.stringify(allKoders, null, 2), err => {
+        if (err) {
+            console.error('ERROR: ', err)
+            res.status(401);
+            res.end()
+        }
+    })
+    // escribir usando fs
+    res.status(201);
+    res.json({
+        message: 'ok'
+    })
+});
+
 
 server.listen(PORT, () => {
     console.log('Server listening in port ' + PORT)
